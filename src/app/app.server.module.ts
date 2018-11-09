ef93027e4d4dc7a01c22293daed958e7cf5468eb
I823622 <jothisubaramaniam.ponnuraj@sap.com>
2018-11-09 15:39:06 -0500

    Add sidenav bar with no content to app-shell

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -5,6 +5,8 @@ import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
 import { Routes, RouterModule } from '@angular/router';
 import { AppShellComponent } from './app-shell/app-shell.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSidenavModule } from '@angular/material';
 
 const routes: Routes = [ { path: 'app-shell-path', component: AppShellComponent }];
 
@@ -13,6 +15,8 @@ const routes: Routes = [ { path: 'app-shell-path', component: AppShellComponent 
     AppModule,
     ServerModule,
     RouterModule.forRoot(routes),
+    NoopAnimationsModule,
+    MatSidenavModule
   ],
   bootstrap: [AppComponent],
   declarations: [AppShellComponent],
