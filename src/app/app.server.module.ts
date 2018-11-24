f52ebfeede2eec3e54b49aa9cc0c2d525dda655f
I823622 <jothisubaramaniam.ponnuraj@sap.com>
2018-11-23 22:06:07 -0500

    fix app ssr app shell + app routing

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -1,13 +1,13 @@
-import { NgModule } from '@angular/core';
+import { NgModule, OnDestroy } from '@angular/core';
 import { ServerModule } from '@angular/platform-server';
 
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
-import { Routes, RouterModule, provideRoutes } from '@angular/router';
+import { Routes, RouterModule, provideRoutes, Router } from '@angular/router';
 import { AppShellComponent } from './app-shell/app-shell.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
-const routes: Routes = [ { path: 'app-shell-path', component: AppShellComponent }];
+const srvRoutes: Routes = [ { path: 'app-shell-path', component: AppShellComponent }];
 
 @NgModule({
   imports: [
@@ -15,8 +15,24 @@ const routes: Routes = [ { path: 'app-shell-path', component: AppShellComponent 
     ServerModule,
     NoopAnimationsModule,
   ],
-  providers: [provideRoutes(routes)],
+  providers: [provideRoutes(srvRoutes)],
   bootstrap: [AppComponent],
   declarations: [AppShellComponent],
 })
-export class AppServerModule {}
+export class AppServerModule implements OnDestroy {
+
+  private appRoutes: Routes;
+
+  constructor(private router: Router) {
+    this.appRoutes = this.router.config;
+    if(this.appRoutes !== srvRoutes) {
+      this.router.resetConfig(srvRoutes);
+    }
+  }
+
+  ngOnDestroy() {
+    if(this.appRoutes !== srvRoutes) {
+      this.router.resetConfig(this.appRoutes);
+    }
+  }
+}
