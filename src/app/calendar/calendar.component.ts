01cd191811ce4590fd511978d3feb87e14cf2d80
I823622 <jothisubaramaniam.ponnuraj@sap.com>
2018-12-14 15:53:39 -0500

    use new Date() and fix time shown on the side

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -17,14 +17,14 @@ export class CalendarComponent {
 
   view: string = 'month';
 
-  viewDate: Date = new Date('2018-12-10');
+  viewDate: Date = new Date()
 
   // exclude weekends
   excludeDays: number[] = [0, 6];
 
   weekStartsOn = DAYS_OF_WEEK.SUNDAY;
 
-  public ownWeekViewHour(date: Date, locale: string, offset: number): string {
-    return formatDate(addMinutes(date, offset), 'h:mm', locale);
+  public ownWeekViewHour(date: Date, locale: string): string {
+    return formatDate(date, 'h:mm', locale);
   }
 }
